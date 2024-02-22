446a67875a7b241f33be317c9da0f9fd28e8a30b
Eden Denis <edendenis@gmail.com>
2024-02-22 09:57:58 -0300

    Update repo

--- README.py
+++ README.py
@@ -1,41 +1,36 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Productio
+# # Corpus
 # 
-# [![DOI](https://zenodo.org/badge/758761955.svg)](https://zenodo.org/doi/10.5281/zenodo.10672810)
+# [![DOI](https://zenodo.org/badge/758776069.svg)](https://zenodo.org/doi/10.5281/zenodo.10673019)
 # 
 
 # ## Resumo
 # 
-# Aplicação Computacional de Productio. A economia moderna permite que as empresas produzam em vários países, aproveitando vantagens como salários e impostos mais baixos. O modelo PRODUCTIO atende empresas nacionais e transnacionais com unidades produtivas espalhadas por diversos países, otimizando estratégias de redução de custos.
+# Aplicação Computacional de Corpus. O programa Corpus calcula o Índice de Massa Corporal (IMC) convertendo os dados de temperatura. Ele avalia as proporções de peso e altura, fornecendo informações sobre os níveis de saúde e condicionamento físico.
 # 
 # ## _Abstract_
 # 
-# _Computational Application of Productio. The modern economy allows companies to produce in multiple countries, leveraging advantages such as lower wages and taxes. The PRODUCTIO model aids national and transnational companies with production units spread across various countries, optimizing cost reduction strategies._
+# _Computational Application of Corpus. The Corpus program calculates Body Mass Index (BMI) by converting temperature data. It assesses weight and height ratios, providing insights into health and fitness levels._
+# 
 
 # ## Revisão(ões)/Versão(ões)
 # 
 # | Revisão número | Data da revisão | Descrição da revisão                                    | Autor da revisão                                |
 # |:--------------:|:---------------:|:--------------------------------------------------------|:------------------------------------------------|
-# | 0              | 16/02/2024      | <ul><li>Revisão inicial/criação do documento.</li></ul> | <ul><li>Eden Denis F. da S. L. Santos</li></ul> |
+# | 0              | 17/02/2024      | <ul><li>Revisão inicial/criação do documento.</li></ul> | <ul><li>Eden Denis F. da S. L. Santos</li></ul> |
 # 
 
 # ## 1. Como executar a aplicação
 # 
-# 1. Abrir o arquivo `main_<nome_da_aplicacao>.ipynb` o qual está com comentários, alterar o banco de dados (existem exemplos de bancos de dados) na pasta que deverá ser utilizado para a execução e executar todas as células. 
+# 1. Abrir o arquivo `main_<nome_da_aplicacao>.xlsm` o qual está com comentários, alterar o banco de dados (existem exemplos de bancos de dados) na pasta que deverá ser utilizado para a execução e executar todas as células. 
 #     
 #     Perceber que o <nome_da_aplicacao> trata-se, redudantemente, do nome da aplicação. Coloquei desta forma, pois quis, por ora, generalizar o arquivo `README.md` para poder criar o repositório de cada uma das aplicações que desenvolvi ao longo da minha carreira.
 
 # ## Referências
 # 
-# [1] KENNEDY, J.. ***Swarm intelligence and data mining***. Springer Verlag, 2006.
+# [1] RICIERI, A. P.. ***Curso matemática aplicada à vida***. Prandiano, São Paulo, 2005.
 # 
-# [2] RAY, T.; LIEW, K. M. A.. ***Swarm metaphor for multiobjective optimization***. Engineering Optimization, v. 34, n. 2, p. 141, 2002.
-# 
-# [3] SHI, Y.; EBERHART, R. C.. ***Empirical study of particle swarm optimization***. Proceedings of the Congress on Evolutionary Computation, Washington, p. 1945, 1999.
-# 
-# [4] RICIERI, A. P.. ***Curso matemática aplicada à vida***. Prandiano, São Paulo, 2005.
-# 
-# [5] SANTOS, E. D. F. da S. L.. ***Curso de excel: módulo v***. EDF Tecnologia, São Paulo, 2011.
+# [2] SANTOS, E. D. F. da S. L.. ***Curso de excel: módulo i***. EDF Tecnologia, São Paulo, 2011.
 # 
